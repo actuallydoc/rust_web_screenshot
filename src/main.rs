5ed951744fb591dd49fdb4f3f4e432de0a1b5d45
ActuallyDoc <actuallydoc@gmail.com>
2022-09-25 22:01:11 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use headless_chrome::{protocol::page::ScreenshotFormat, Browser, LaunchOptionsBuilder};
+use std::fs;
+use std::io::stdin;
+use std::thread::sleep;
+use std::time::Duration;
+//First cli app that i made for screen shotting a website and saving it to a file
+fn main() {
+    let mut s = String::new();
+    let mut x_cords = String::new();
+    let mut y_cords = String::new();
+    println!("Enter URL: ");
+    stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    println!("Enter X size pixles: ");
+
+    stdin()
+        .read_line(&mut x_cords)
+        .expect("Did not enter a correct string");
+
+    println!("Enter Y size pixles: ");
+
+    stdin()
+        .read_line(&mut y_cords)
+        .expect("Did not enter a correct string");
+    take_screenshot(s, x_cords, y_cords).unwrap();
+
+    println!("Done");
+}
+
+fn take_screenshot(
+    url: String,
+    x_size: String,
+    y_size: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Taking screenshot of {}", url);
+    println!("X size: {}", x_size);
+    println!("Y size: {}", y_size);
+
+    let x_size: u32 = x_size.trim().parse().unwrap();
+    let y_size: u32 = y_size.trim().parse().unwrap();
+    let options = LaunchOptionsBuilder::default()
+        .window_size(Some((x_size, y_size)))
+        .build()?;
+    let browser = Browser::new(options)?;
+
+    let tab = browser.wait_for_initial_tab()?;
+
+    tab.navigate_to(&url)?;
+
+    tab.wait_until_navigated()?;
+
+    sleep(Duration::from_secs(3));
+
+    let png_data = tab.capture_screenshot(ScreenshotFormat::PNG, None, true)?;
+
+    fs::write("screenshot.png", png_data)?;
+
+    Ok(())
+}
